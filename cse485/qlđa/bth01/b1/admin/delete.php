70f11c9cbfac607e2491ed74c2952a06cde2c633
ngvanntienn <ngvanntienn05@gmail.com>
2025-11-26 16:44:13 +0700

    Delete cse485/qlđa/bth01 directory

--- cse485/qlđa/bth01/b1/admin/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "../connect.php";
-    require '../data.php';
-    $this_id = $_GET['id'];
-    $sql = "DELETE FROM loai_hoa WHERE id = $this_id";
-    mysqli_query($conn, $sql);
-    header("Location: ../index.php?page=admin");
-?>
