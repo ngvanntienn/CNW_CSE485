f6571caf1f2d00a02371e4125d67f8964c3e5b80
ngvanntienn <ngvanntienn05@gmail.com>
2025-11-26 20:06:05 +0700

    Update index.php

--- cse485/bth01/b2/index.php
+++ cse485/bth01/b2/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bài thi trắc nghiệm</title>
+    <title>Bài 2</title>
     <style>
         body {
             font-family: 'Segoe UI',sans-serif;
@@ -133,5 +133,6 @@
     </div>
 </body>
 </html>
+
 
 
